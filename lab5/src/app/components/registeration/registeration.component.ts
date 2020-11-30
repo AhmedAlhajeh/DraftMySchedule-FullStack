c0eea7baedd9887e6f389840cf71e9141acd55f0
AhmedAlhajeh <ahmad.f.alhajeh@gmail.com>
2020-11-30 17:26:40 -0500

    Verification of email for a new account

--- lab5/src/app/components/registeration/registeration.component.ts
+++ lab5/src/app/components/registeration/registeration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registeration',
@@ -10,35 +11,50 @@ export class RegisterationComponent implements OnInit {
   UserName!: String;
   Email!: String;
   Password!: String;
+  Verification!: String;
+
   URI= "http://localhost:3000/"
+  
+  constructor(private http: HttpClient, private route: Router) { }
 
-  constructor(private http: HttpClient) { }
-
+  visible: boolean = false; 
   register(): void {
     var UsernameInformation = {
       UserName: this.UserName,
       Email: this.Email,
-      Password: this.Password
+      Password: this.Password,
+      
     }
+    
     this.http.post<any>(this.URI + 'register', UsernameInformation).subscribe(data => {
+      
       if (data.message == 'You have been registered!'){
         alert("You have been registered");
-        let registeration = document.getElementById("Register");
-        registeration?.setAttribute("id", "Register");
-        let verified = document.createElement("button");
-        let link = document.createTextNode("Verify Your Email");
-        document.body.insertBefore(verified, document.getElementById("Register"));
-        registeration?.appendChild(verified);
-        verified.appendChild(link);
+        this.visible = true;
+
+      }else if(data.message == 'The email is already exist!'){
+        alert("The email is already exist!");
       }
-       
     })
-    
 
 
   }
 
+  verification(): void {
+      var UsernameInformationForVerification = {
+      UserName: this.UserName,
+      Email: this.Email,
+      Password: this.Password,
+      Verification: this.Verification
+      }
+      this.http.put<any>(this.URI + 'verification', UsernameInformationForVerification).subscribe(data => {
+        if(data.message == 'Verified'){
+          this.route.navigate(['/verified']);
+        }
+        
 
+      })
+  }
 
   ngOnInit(): void {
   }
