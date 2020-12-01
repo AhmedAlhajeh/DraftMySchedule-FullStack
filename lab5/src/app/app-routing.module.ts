a794002ac0eec91c4368489ed24c474e6a20111c
AhmedAlhajeh <ahmad.f.alhajeh@gmail.com>
2020-12-01 01:57:06 -0500

    Local authentication which uses email and password as credentials

--- lab5/src/app/app-routing.module.ts
+++ lab5/src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CreatingComponent } from './components/creating/creating.component';
+import { ManagingComponent } from './components/managing/managing.component';
+import { LoginComponent} from './components/login/login.component';
+import { RegisterationComponent} from './components/registeration/registeration.component';
+import { BrowsingComponent} from './components/browsing/browsing.component';
+import { VerifiedComponent} from './components/verified/verified.component';
+import { AdminComponent} from './components/admin/admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+{path: 'Home', component: HomeComponent},
+{path: 'creating', component: CreatingComponent},
+{path: 'managing', component: ManagingComponent},
+{path: 'browsing', component: BrowsingComponent},
+{path: 'registeration', component: RegisterationComponent},
+{path: 'verified', component: VerifiedComponent},
+{path: 'admin', component: AdminComponent},
+{path: '', redirectTo: 'Home', pathMatch: 'full'}//making sure the first page shows up when I load my website is the home page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [HomeComponent, CreatingComponent, ManagingComponent, LoginComponent, RegisterationComponent, AdminComponent, VerifiedComponent]
--- lab5/src/app/app.module.ts
+++ lab5/src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { CreatingComponent } from './components/creating/creating.component';
+import { ManagingComponent } from './components/managing/managing.component';
+import { LoginComponent } from './components/login/login.component';
+import { BrowsingComponent } from './components/browsing/browsing.component';
+import { AuthenticatedNavbarComponent } from './components/authenticated-navbar/authenticated-navbar.component';
+import { VerifiedComponent } from './components/verified/verified.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CreatingComponent,
+    ManagingComponent,
+    routingComponents,
+    LoginComponent,
+    BrowsingComponent,
+    AuthenticatedNavbarComponent,
+    VerifiedComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
